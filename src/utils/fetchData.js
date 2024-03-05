7d66a28477a582f6899e588cb3e7a8270b47fa33
abhi111456 <abhishek.21jics039@jietjodhpur.ac.in>
2024-03-05 23:07:06 +0530

    A

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,13 @@ export const exerciseOptions={
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'e0bd9c65b5msh6f502da7d93e949p1fb7f6jsn20a2f23e1359',
+    },
+  };
 export const fetchData = async (url, options) => {
     const res = await fetch(url, options);
     const data = await res.json();
